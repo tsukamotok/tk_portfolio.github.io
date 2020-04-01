c2ead8bba10b8e27fa9201f185014a9576b2cb90
塚本 恭輔 <tsukamotok@istyle.local>
2020-04-01 17:49:33 +0900

    メニューモーダル表示時の背景の固定

--- js/script.js
+++ js/script.js
@@ -20,3 +20,30 @@ function toggleNav() {
   }
   
   toggleNav();
+
+
+// フォームの値を取得
+function onButtonClick() {
+  const send = document.getElementById('js-send-done');
+  const body = document.body;
+  send.classList.toggle('send-modal-open');
+  document.getElementById('js-header').classList.toggle('header-open');
+
+  const company = document.getElementsByName('company');
+  const name = document.getElementsByName('name');
+  const eMail = document.getElementsByName('e-mail');
+  const tel = document.getElementsByName('tel');
+  const message = document.getElementsByName('message');
+
+  const contact = {
+    company: company[0].value,
+    name: name[0].value,
+    eMail: eMail[0].value,
+    tel: tel[0].value,
+    message: message[0].value
+  };
+
+  for(let p in contact) {
+    console.log(p + ':' + contact[p]);
+  }
+}
